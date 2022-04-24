00de7174765488925fa9598791f71cf51cadc141
Paula Faidley <faidleyp@etsu.edu>
2022-04-23 23:02:52 -0400

    fixed css

---
+++ Pages/assets/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScavengersRUs.Pages.assets
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
