82ce5cc50e2a10f4980a1fbd51b5f0e4fc311f7f
Paula Faidley <faidleyp@etsu.edu>
2022-04-26 20:30:34 -0400

    CSS, need to fix links

---
+++ Pages/FAQ.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScavengersRUs.Pages
+{
+    public class FAQModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
